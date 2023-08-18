002b6058452de4495ced7f3c8ca0152ee5888d78
OnClick <2809399366@qq.com>
2023-08-19 01:05:57 +0800

    修正拷贝

--- Assets/WooAsset/Editor/Build/Task/CopyBundlesToStreamTask.cs
+++ Assets/WooAsset/Editor/Build/Task/CopyBundlesToStreamTask.cs
@@ -24,6 +24,7 @@ namespace WooAsset
                   AssetsHelper.CombinePath(destPath, StreamBundleList.fileName),
                   true
                   );
+                base.Done();
             }
 
             protected override string GetDestFileName(string src)
