634808070649321d7aea4e80f8decf84d5e21f87
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2024-05-28 10:44:40 +0800

    拆开

--- Assets/WooAsset/Runtime/Operation/Load/Asset/Asset.cs
+++ Assets/WooAsset/Runtime/Operation/Load/Asset/Asset.cs
@@ -4,34 +4,6 @@ using System.Linq;
 using Object = UnityEngine.Object;
 namespace WooAsset
 {
-    public class RawAsset : AssetHandle<RawObject>
-    {
-        public RawAsset(AssetLoadArgs loadArgs) : base(loadArgs)
-        {
-        }
-        public RawObject GetAsset() => isDone ? value : null;
-
-        public override float progress
-        {
-            get
-            {
-                if (isDone) return 1;
-                return bundle.progress;
-            }
-        }
-        protected override void InternalLoad()
-        {
-            if (bundle.isErr)
-            {
-                this.SetErr(bundle.error);
-                InvokeComplete();
-                return;
-            }
-
-            var raw = bundle.LoadRawObject(path);
-            SetResult(raw);
-        }
-    }
     public class Asset : AssetHandle<UnityEngine.Object>
     {
 
