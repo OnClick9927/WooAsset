c964ddaff1791866c9247cb61771ac3b231d64d1
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2024-11-28 16:59:36 +0800

    去掉多余的

--- Assets/WooAsset/Editor/Build/Pack/IAssetSelector.cs
+++ Assets/WooAsset/Editor/Build/Pack/IAssetSelector.cs
@@ -65,17 +65,6 @@ namespace WooAsset
         }
     }
 
-
-    [AssetSelector(AssetSelectorParamType.None)]
-
-    public class ShaderSelector : IAssetSelector
-    {
-        public List<EditorAssetData> Select(List<EditorAssetData> assets, AssetSelectorParam param)
-        {
-            return assets.FindAll(x => x.type == AssetType.Shader || x.type == AssetType.ShaderVariant);
-        }
-    }
-
     [AssetSelector(AssetSelectorParamType.Path)]
     public class DirectoryTopSelector : IAssetSelector
     {
