7be384d407232134d7049c78ca67da0f8cd17e24
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2025-03-06 20:38:33 +0800

    GUI

---
+++ Editor/Window/PropertyDrawer/TagAssetsDawer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace WooAsset
+{
+    [CustomPropertyDrawer(typeof(TagAssets), true)]
+    class TagAssetsDawer : UnityEditor.PropertyDrawer
+    {
+        static GUIContent empty = new GUIContent();
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var assets = property.FindPropertyRelative(nameof(TagAssets.assets));
+            var _base = base.GetPropertyHeight(assets, label);
+            if (!assets.isExpanded)
+                return _base;
+            return assets.arraySize * _base + 60;
+        }
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var rs = AssetsWindow.RectEx.VerticalSplit(position, 150);
+            var tag = property.FindPropertyRelative(nameof(TagAssets.tag));
+            var assets = property.FindPropertyRelative(nameof(TagAssets.assets));
+            if (!assets.isExpanded)
+                EditorGUI.PropertyField(rs[0], tag, empty);
+            else
+            {
+                var rss = AssetsWindow.RectEx.HorizontalSplit(rs[0], 20);
+                EditorGUI.PropertyField(rss[0], tag, empty);
+            }
+            //GUI.enabled = false;
+            EditorGUI.PropertyField(rs[1], assets, empty);
+            //GUI.enabled = true;
+
+        }
+    }
+}
---
+++ Editor/Window/PropertyDrawer/TagAssets_TagDataDawer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace WooAsset
+{
+    [CustomPropertyDrawer(typeof(TagAssets.TagData), true)]
+    class TagAssets_TagDataDawer : UnityEditor.PropertyDrawer
+    {
+        static GUIContent empty = new GUIContent();
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            var rs = AssetsWindow.RectEx.VerticalSplit(position, 100);
+            var type = property.FindPropertyRelative(nameof(TagAssets.TagData.type));
+            var path = property.FindPropertyRelative(nameof(TagAssets.TagData.path));
+
+            EditorGUI.PropertyField(rs[0], type, empty);
+            EditorGUI.PropertyField(rs[1], path, empty);
+            GUI.enabled = true;
+
+        }
+    }
+}
